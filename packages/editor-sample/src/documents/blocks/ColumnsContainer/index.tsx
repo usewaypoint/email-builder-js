28027b9fc47606d9caf2c40e921ad241a376f9eb
cohitre <carlosrr@gmail.com>
2024-02-28 10:37:54 -0800

    Consuming @usewaypoint/block-columns-container

--- packages/editor-sample/src/documents/blocks/ColumnsContainer/index.tsx
+++ packages/editor-sample/src/documents/blocks/ColumnsContainer/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { ColumnsContainer as BaseColumnsContainer } from '@usewaypoint/block-columns-container';
+
 import { TEditorBlock } from '../../editor/core';
 import { useCurrentBlockId } from '../../editor/EditorBlock';
 import { useEditorState } from '../../editor/EditorContext';
@@ -8,70 +10,55 @@ import EditorChildrenIds from '../helpers/EditorChildrenIds';
 
 import ColumnsContainerPropsSchema, { ColumnsContainerProps } from './ColumnsContainerPropsSchema';
 
-export function ColumnsContainer({ props: { columnsCount, columns } }: ColumnsContainerProps) {
-  let lastColumn = null;
-  if (columnsCount === 3) {
-    lastColumn = (
-      <td>
-        {columns[2].childrenIds.map((childId) => (
-          <ReaderBlock key={childId} id={childId} />
-        ))}
-      </td>
-    );
+export function ColumnsContainer({ style, props }: ColumnsContainerProps) {
+  const { columns, ...restProps } = props ?? {};
+  let cols = undefined;
+  if (columns) {
+    cols = columns.map((col) => col.childrenIds.map((childId) => <ReaderBlock key={childId} id={childId} />));
   }
-  return (
-    <table
-      align="center"
-      width="100%"
-      cellPadding="0"
-      border={0}
-      style={{ tableLayout: 'fixed', borderCollapse: 'collapse' }}
-    >
-      <tbody style={{ width: '100%' }}>
-        <tr style={{ width: '100%' }}>
-          <td>
-            {columns[0].childrenIds.map((childId) => (
-              <ReaderBlock key={childId} id={childId} />
-            ))}
-          </td>
-          <td>
-            {columns[1].childrenIds.map((childId) => (
-              <ReaderBlock key={childId} id={childId} />
-            ))}
-          </td>
-          {lastColumn}
-        </tr>
-      </tbody>
-    </table>
-  );
+
+  return <BaseColumnsContainer props={restProps} columns={cols} style={style} />;
 }
 
-export function EditorColumnsContainer(data: ColumnsContainerProps) {
-  const { columnsCount, columns } = data.props;
+const EMPTY_COLUMNS = [{ childrenIds: [] }, { childrenIds: [] }, { childrenIds: [] }];
+
+export function EditorColumnsContainer({ style, props }: ColumnsContainerProps) {
   const [{ document }, setEditorState] = useEditorState();
   const blockId = useCurrentBlockId();
+
+  const { columns, ...restProps } = props ?? {};
+  const columnsValue = columns ?? EMPTY_COLUMNS;
+
+  const renderColumn = (columnIndex: 0 | 1 | 2) => (
+    <EditorChildrenIds
+      childrenIds={columns?.[columnIndex]?.childrenIds || []}
+      insertBlock={(block, index) => {
+        insertBlock(columnIndex, block, index);
+      }}
+    />
+  );
 
   const insertBlock = (columnIndex: 0 | 1 | 2, blockConfiguration: TEditorBlock, i: number | null) => {
     const id = `block-${Date.now()}`;
 
     const getColumns = () => {
-      const columnsCopy: ColumnsContainerProps['props']['columns'] = [...columns];
+      const columnsCopy = [...columnsValue];
       if (i === null) {
         columnsCopy[columnIndex] = {
-          childrenIds: [...columns[columnIndex].childrenIds, id],
+          childrenIds: [...columnsValue[columnIndex].childrenIds, id],
         };
         return columnsCopy;
       }
       columnsCopy[columnIndex] = {
         childrenIds: [
-          ...columns[columnIndex].childrenIds.slice(0, i),
+          ...columnsValue[columnIndex].childrenIds.slice(0, i),
           id,
-          ...columns[columnIndex].childrenIds.slice(i),
+          ...columnsValue[columnIndex].childrenIds.slice(i),
         ],
       };
       return columnsCopy;
     };
-    const data = ColumnsContainerPropsSchema.parse(document[blockId].data);
+
     setEditorState({
       selectedBlockId: id,
       document: {
@@ -79,60 +66,23 @@ export function EditorColumnsContainer(data: ColumnsContainerProps) {
         [id]: blockConfiguration,
         [blockId]: {
           type: 'ColumnsContainer',
-          data: {
-            ...data,
+          data: ColumnsContainerPropsSchema.parse({
+            style,
             props: {
-              ...data.props,
+              ...restProps,
               columns: getColumns(),
             },
-          },
+          }),
         },
       },
     });
   };
 
-  let lastColumn = null;
-  if (columnsCount === 3) {
-    lastColumn = (
-      <td>
-        <EditorChildrenIds
-          childrenIds={columns[2].childrenIds}
-          insertBlock={(block, index) => {
-            insertBlock(2, block, index);
-          }}
-        />
-      </td>
-    );
-  }
   return (
-    <table
-      align="center"
-      width="100%"
-      cellPadding="0"
-      border={0}
-      style={{ tableLayout: 'fixed', borderCollapse: 'collapse' }}
-    >
-      <tbody style={{ width: '100%' }}>
-        <tr style={{ width: '100%' }}>
-          <td>
-            <EditorChildrenIds
-              childrenIds={columns[0].childrenIds}
-              insertBlock={(block, index) => {
-                insertBlock(0, block, index);
-              }}
-            />
-          </td>
-          <td>
-            <EditorChildrenIds
-              childrenIds={columns[1].childrenIds}
-              insertBlock={(block, index) => {
-                insertBlock(1, block, index);
-              }}
-            />
-          </td>
-          {lastColumn}
-        </tr>
-      </tbody>
-    </table>
+    <BaseColumnsContainer
+      props={restProps}
+      style={style}
+      columns={[renderColumn(0), renderColumn(1), renderColumn(2)]}
+    />
   );
 }
