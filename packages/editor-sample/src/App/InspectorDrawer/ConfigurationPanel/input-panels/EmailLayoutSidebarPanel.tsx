b3bf1911322325700191583fe2b736b2112ee598
Jordan Isip <me@jordanisip.com>
2024-02-29 15:15:11 -0500

    Removing secondary swatch set on color pickers

--- packages/editor-sample/src/App/InspectorDrawer/ConfigurationPanel/input-panels/EmailLayoutSidebarPanel.tsx
+++ packages/editor-sample/src/App/InspectorDrawer/ConfigurationPanel/input-panels/EmailLayoutSidebarPanel.tsx
@@ -10,13 +10,11 @@ import ColorInput from './helpers/inputs/ColorInput';
 
 type EmailLayoutSidebarPanelProps = z.infer<typeof EmailLayoutPropsSchema>;
 
-const SECONDARY_SWATCH: string[] = [];
 type EmailLayoutSidebarFieldsProps = {
   data: EmailLayoutSidebarPanelProps;
   setData: (v: EmailLayoutSidebarPanelProps) => void;
 };
 export default function EmailLayoutSidebarFields({ data, setData }: EmailLayoutSidebarFieldsProps) {
-  const secondarySwatch = SECONDARY_SWATCH;
   const [, setErrors] = useState<Zod.ZodError | null>(null);
 
   const updateData = (d: unknown) => {
@@ -35,20 +33,17 @@ export default function EmailLayoutSidebarFields({ data, setData }: EmailLayoutS
         label="Backdrop color"
         defaultValue={data.backdropColor}
         onChange={(backdropColor) => updateData({ ...data, backdropColor })}
-        secondarySwatch={secondarySwatch}
       />
       <ColorInput
         label="Canvas color"
         defaultValue={data.canvasColor}
         onChange={(canvasColor) => updateData({ ...data, canvasColor })}
-        secondarySwatch={secondarySwatch}
       />
       <Divider />
       <ColorInput
         label="Text color"
         defaultValue={data.textColor}
         onChange={(textColor) => updateData({ ...data, textColor })}
-        secondarySwatch={secondarySwatch}
       />
     </BaseSidebarPanel>
   );
