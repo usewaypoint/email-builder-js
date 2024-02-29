50083086cf51c13dc13b90698755f0608dfa692e
cohitre <carlosrr@gmail.com>
2024-02-29 10:51:20 -0800

    HTML/JSON syntax highlighting

--- packages/editor-sample/src/App/TemplatePanel/helper/TextEditorPanel.tsx
+++ packages/editor-sample/src/App/TemplatePanel/helper/TextEditorPanel.tsx
@@ -1,22 +1,37 @@
-import * as React from 'react';
+import hljs from 'highlight.js';
+import jsonHighlighter from 'highlight.js/lib/languages/json';
+import xmlHighlighter from 'highlight.js/lib/languages/xml';
+import { html } from 'js-beautify';
+import React, { useMemo } from 'react';
+
+hljs.registerLanguage('json', jsonHighlighter);
+hljs.registerLanguage('html', xmlHighlighter);
 
 type TextEditorPanelProps = {
+  type: 'json' | 'html';
   value: string;
 };
-export default function TextEditorPanel({ value }: TextEditorPanelProps) {
+export default function TextEditorPanel({ type, value }: TextEditorPanelProps) {
+  const code = useMemo(() => {
+    switch (type) {
+      case 'html':
+        return hljs.highlight(html(value), { language: 'html' });
+      default:
+        return hljs.highlight(value, { language: 'json' });
+    }
+  }, [type, value]);
+
   return (
-    <textarea
-      style={{
-        padding: 16,
-        resize: 'none',
-        borderWidth: 0,
-        height: '100%',
-        width: '100%',
-        outline: 0,
-        boxSizing: 'border-box',
+    <pre
+      style={{ margin: 0, padding: 16 }}
+      dangerouslySetInnerHTML={{ __html: code.value }}
+      onClick={(ev) => {
+        const s = window.getSelection();
+        if (s === null) {
+          return;
+        }
+        s.selectAllChildren(ev.currentTarget);
       }}
-    >
-      {value}
-    </textarea>
+    />
   );
 }
