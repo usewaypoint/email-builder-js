ed4d5bca6cbba31fdb4dfb03e62fb6701eecb2e4
cohitre <carlosrr@gmail.com>
2024-02-29 06:25:43 -0800

    Fixing build

--- packages/editor-sample/src/App/TemplatePanel/JsonPanel.tsx
+++ packages/editor-sample/src/App/TemplatePanel/JsonPanel.tsx
@@ -1,11 +1,11 @@
 import React, { useMemo } from 'react';
 
-import { useEditorState } from '../../documents/editor/EditorContext';
+import { useDocument } from '../../documents/editor/EditorContext';
 
 import TextEditorPanel from './helper/TextEditorPanel';
 
 export default function JsonPanel() {
-  const [{ document }] = useEditorState();
+  const document = useDocument();
   const string = useMemo(() => JSON.stringify(document, null, '  '), [document]);
   return <TextEditorPanel value={string} />;
 }
