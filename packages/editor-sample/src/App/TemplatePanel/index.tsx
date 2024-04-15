1aca411f5663c0df881ac2bf3bc43787dcb2a5f6
Jordan Isip <jordan@usewaypoint.com>
2024-04-15 11:00:18 -0400

    Adding download JSON button (#85)

--- packages/editor-sample/src/App/TemplatePanel/index.tsx
+++ packages/editor-sample/src/App/TemplatePanel/index.tsx
@@ -14,6 +14,7 @@ import {
 import ToggleInspectorPanelButton from '../InspectorDrawer/ToggleInspectorPanelButton';
 import ToggleSamplesPanelButton from '../SamplesDrawer/ToggleSamplesPanelButton';
 
+import DownloadJson from './DownloadJson';
 import HtmlPanel from './HtmlPanel';
 import ImportJson from './ImportJson';
 import JsonPanel from './JsonPanel';
@@ -94,6 +95,7 @@ export default function TemplatePanel() {
             <MainTabsGroup />
           </Stack>
           <Stack direction="row" spacing={2}>
+            <DownloadJson />
             <ImportJson />
             <ToggleButtonGroup value={selectedScreenSize} exclusive size="small" onChange={handleScreenSizeChange}>
               <ToggleButton value="desktop">
