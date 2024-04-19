1245bdac807e0989babcd4b2db1bc3a2a3bbf376
cohitre <carlosrr@gmail.com>
2024-04-19 08:37:11 -0700

    Adding react-dom as a peer dependency (#91)

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  transformIgnorePatterns: ['/node_modules/(?!(@usewaypoint)/)'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'ts-jest',
+  },
 };
 
 export default config;
---
+++ packages/email-builder/src/renderers/renderToStaticMarkup.spec.tsx
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment node
+ */
+
+import renderToStaticMarkup from './renderToStaticMarkup';
+
+describe('renderToStaticMarkup', () => {
+  it.only('renders into a string', () => {
+    const result = renderToStaticMarkup(
+      {
+        root: {
+          type: 'Container',
+          data: {
+            props: {
+              childrenIds: [],
+            },
+          },
+        },
+      },
+      { rootBlockId: 'root' }
+    );
+    expect(result).toEqual('<!DOCTYPE html><html><body><div></div></body></html>');
+  });
+});
