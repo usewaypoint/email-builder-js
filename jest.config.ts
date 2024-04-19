2b1479e6405cbff5c2ff0efdc7de6210f1a0f1d7
cohitre <carlosrr@gmail.com>
2024-04-19 10:33:58 -0700

    Releasing packages with cjs and modules (#93)

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,6 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  transformIgnorePatterns: ['/node_modules/(?!(@usewaypoint)/)'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'ts-jest',
-  },
 };
 
 export default config;
