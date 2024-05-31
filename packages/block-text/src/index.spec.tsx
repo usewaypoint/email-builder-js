e00fd580142ab364ae4a8fe7dd23b3b9dcc3fbbc
cohitre <carlosrr@gmail.com>
2024-05-31 09:28:53 -0700

    Adding markdown to block-text (#106)

--- packages/block-text/src/index.spec.tsx
+++ packages/block-text/src/index.spec.tsx
@@ -8,4 +8,29 @@ describe('block-text', () => {
   it('renders with default values', () => {
     expect(render(<Text />).asFragment()).toMatchSnapshot();
   });
+
+  it('renders with safe markdown', () => {
+    expect(
+      render(
+        <Text
+          props={{
+            text: `## This is <span>markdown</span>`,
+            markdown: true,
+          }}
+        />
+      ).asFragment()
+    ).toMatchSnapshot();
+  });
+
+  it('renders without markdown', () => {
+    expect(
+      render(
+        <Text
+          props={{
+            text: `## This is not <span>markdown</span>`,
+          }}
+        />
+      ).asFragment()
+    ).toMatchSnapshot();
+  });
 });
