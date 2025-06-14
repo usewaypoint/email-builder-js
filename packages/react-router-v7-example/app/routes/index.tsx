91e3a0223f66578ede005b868936addbd2e2721f
gjc14 <yin@yinc.me>
2025-06-15 07:22:24 +0800

    feat: init react router v7

---
+++ packages/react-router-v7-example/app/routes/index.tsx
@@ -0,0 +1,12 @@
+import type { Route } from "./+types/index";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <>Start</>;
+}
