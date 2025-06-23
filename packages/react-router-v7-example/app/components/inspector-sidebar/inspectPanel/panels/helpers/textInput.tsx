1263440d6bc31be8efb61fcc41469b34c78c4553
gjc14 <yin@yinc.me>
2025-06-24 02:21:45 +0800

    feat: textarea input

--- packages/react-router-v7-example/app/components/inspector-sidebar/inspectPanel/panels/helpers/textInput.tsx
+++ packages/react-router-v7-example/app/components/inspector-sidebar/inspectPanel/panels/helpers/textInput.tsx
@@ -1,5 +1,6 @@
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
+import { Textarea } from '~/components/ui/textarea';
 import { cn } from '~/lib/utils';
 
 type TextInputProps = React.ComponentProps<typeof Input> & {
@@ -23,6 +24,31 @@ export function TextInput({ label, className, ...props }: TextInputProps) {
     <div className={cn('space-y-1', className)}>
       <Label>{label}</Label>
       <Input {...props} />
+    </div>
+  );
+}
+
+type TextareaInputProps = React.ComponentProps<typeof Textarea> & {
+  label?: string;
+};
+
+/**
+ * @example
+ * ```tsx
+ * <TextareaInput
+ *   label={'Textarea'}
+ *   value={text}
+ *   onChange={(e) => {
+ *     updateData({ ...data, props: { ...data.props, text: e.target.value } });
+ *   }}
+ * />
+ * ```
+ */
+export function TextareaInput({ label, className, ...props }: TextareaInputProps) {
+  return (
+    <div className={cn('space-y-1', className)}>
+      <Label>{label}</Label>
+      <Textarea {...props} />
     </div>
   );
 }
