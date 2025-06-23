b81d94126344d69b66d37f1ee729fffe35757324
gjc14 <yin@yinc.me>
2025-06-24 00:53:20 +0800

    feat: style input helper

---
+++ packages/react-router-v7-example/app/components/inspector-sidebar/inspectPanel/panels/helpers/styleInput.tsx
@@ -0,0 +1,111 @@
+import { AlignCenter, AlignLeft, AlignRight, AlignStartHorizontal } from 'lucide-react';
+import { Label } from '~/components/ui/label';
+import type { TStyle } from '~/documents/blocks/helpers/TStyle';
+import { cn } from '~/lib/utils';
+import { ToggleGroupInput } from './radioInput';
+import { SliderInput } from './sliderInput';
+
+type StyleInputProps = {
+  select: { style: keyof TStyle; label: string }[];
+  className?: string;
+  value?: Partial<TStyle> | null;
+  onChange?(value: Partial<TStyle>): void;
+};
+
+export function StyleInput(props: StyleInputProps) {
+  function handleStyleChange<T>(key: keyof TStyle, value: T) {
+    const newStyle = { ...props.value, [key]: value };
+    if (props.onChange) {
+      props.onChange(newStyle);
+    }
+  }
+
+  return (
+    <div className={cn('space-y-5', props.className)}>
+      {props.select.map((item) => {
+        switch (item.style) {
+          case 'backgroundColor':
+            break;
+
+          case 'borderColor':
+            break;
+
+          case 'borderRadius':
+            break;
+
+          case 'color':
+            break;
+
+          case 'fontFamily':
+            break;
+
+          case 'fontSize':
+            break;
+
+          case 'fontWeight':
+            break;
+
+          case 'padding': {
+            const paddingValue = props.value?.padding || { top: 0, bottom: 0, left: 0, right: 0 };
+            const icons = {
+              top: <AlignStartHorizontal className="w-4 h-4" />,
+              bottom: <AlignStartHorizontal className="w-4 h-4 transform rotate-180" />,
+              left: <AlignStartHorizontal className="w-4 h-4 transform rotate-90" />,
+              right: <AlignStartHorizontal className="w-4 h-4 transform -rotate-90" />,
+            };
+
+            return (
+              <div key={item.style} className="space-y-1">
+                <Label className="text-sm font-medium">{item.label}</Label>
+                <div className="space-y-0.1">
+                  <div className="w-full flex flex-col items-start gap-1">
+                    {Object.entries(paddingValue).map(([k, v]) => {
+                      return (
+                        <SliderInput
+                          unit="px"
+                          step={1}
+                          min={0}
+                          max={100}
+                          value={v}
+                          onChange={(value) => {
+                            handleStyleChange('padding', {
+                              ...props.value?.padding,
+                              [k]: value,
+                            });
+                          }}
+                          labelHidden
+                          icon={icons[k as keyof typeof icons]}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            );
+          }
+
+          case 'textAlign':
+            return (
+              <ToggleGroupInput
+                key={item.style}
+                variant={'outline'}
+                type="single"
+                options={[
+                  { label: <AlignLeft />, value: 'left' },
+                  { label: <AlignCenter />, value: 'center' },
+                  { label: <AlignRight />, value: 'right' },
+                ]}
+                label={item.label}
+                value={props.value?.textAlign ?? 'left'}
+                onValueChange={(textAlign) => {
+                  handleStyleChange('textAlign', textAlign);
+                }}
+              />
+            );
+        }
+      })}
+    </div>
+  );
+
+  return;
+}
--- packages/react-router-v7-example/app/documents/blocks/helpers/TStyle.ts
+++ packages/react-router-v7-example/app/documents/blocks/helpers/TStyle.ts
@@ -1,13 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 export type TStyle = {
-  backgroundColor?: any;
-  borderColor?: any;
-  borderRadius?: any;
-  color?: any;
-  fontFamily?: any;
-  fontSize?: any;
-  fontWeight?: any;
-  padding?: any;
-  textAlign?: any;
+  backgroundColor?: string; // e.g. '#ffffff'
+  borderColor?: string; // e.g. '#000000'
+  borderRadius?: number; // e.g. 4 (for 4px)
+  color?: string; // e.g. '#333333'
+  fontFamily?: string; // e.g. 'Arial, sans-serif'
+  fontSize?: number; // e.g. 16 (for 16px)
+  fontWeight?: number; // e.g. 400 (for normal weight) or 700 (for bold)
+  padding?: { top: number; bottom: number; right: number; left: number } | null; // e.g. { top: 10, bottom: 10, right: 15, left: 15 }
+  textAlign?: 'center' | 'left' | 'right' | null;
 };
